d9ba611516359d74cc7a27a8ed721827bb4e0424
N00B HACK3R - TALHA <102674297+Talha-Niloy-Cyber@users.noreply.github.com>
2022-04-04 00:47:25 +0600

    Create Talhabomb.py

---
+++ Talhabomb.py
@@ -0,0 +1,16 @@
+print('''\n\033[1;31m
+
+ _______ _______ _____   _______ _______ 
+|_     _|   _   |     |_|   |   |   _   |
+  |   | |       |       |       |       |
+  |___| |___|___|_______|___|___|___|____
+  
+ SMS BOMBING TOOL COMEING SOON:)
+ 
+ DEVLOPER:TALHA ZUBAYER (N00B HACK3R)
+ 
+ FACEBOOK-www.facebook.com/00.x.u.b.a.y.e.r.00
+ 
+ PHONE-01967568140
+ 
+\033[0m''')
